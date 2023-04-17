46c4badf69fdcc2dada0f9dc7e98c3f3cd79b20f
devmicro <bhattrahul270@gmail.com>
2023-04-17 22:55:18 +0530

    Blank project with jest dependency

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <form>
+        <input type="email" placeholder="Email address" />
+        <input type="submit" />
+      </form>
+      <button>Continue with Google</button>
+      <button>Continue with Facebook</button>
+      <button>Continue with Appple ID</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/__tests__/global.test.ts
@@ -0,0 +1,5 @@
+test("It should be working", () => {
+  expect(true).toBe(true);
+});
+
+export default {};
